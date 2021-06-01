42afd1ffcd1d6f3867dbc2acd6b527f31eb01e53
Joshua Fletcher <joshua.f1998@gmail.com>
2021-06-01 13:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#239234";
 
 document.addEventListener("Keydown", function(e) {
-  console.log(e.key);
-});
+  if (e.key == "ArrowLeft" ) {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+      dodger.style.left = `${left - 1}px`;
+  }
+}
